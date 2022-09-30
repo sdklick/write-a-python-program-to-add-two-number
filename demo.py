4b12de8c60b25b857cbf64d84581448b2fdff190
Sumanta Das <sumantad827@gmail.com>
2022-09-30 12:10:46 +0530

    initial commit

---
+++ demo.py
@@ -0,0 +1,8 @@
+# write a python program to add two number
+
+num1=int(input("please input 1st number : "))
+num2=int(input("please enter 2nd number : "))
+
+sum=num1+num2
+
+print(num1,"+",num2,"=",sum)
